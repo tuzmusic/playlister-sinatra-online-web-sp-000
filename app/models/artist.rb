9775abc73f2dc5d5a00b9adbb70d12eb2e7d2232
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-25 16:51:24 -0500

    implement Slugifiable#slug

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,9 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+
+  require_relative './concerns/slugifiable'
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+
 end
